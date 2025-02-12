2f3a67006eb4cdb483a7f211311821edc29afde2
악덕 <rudejrqkr97@gmail.com>
2025-02-13 02:06:04 +0900

    feat: WebApplicationServer 클래스에 메인 메소드 추가
    - Tomcat 서버를 시작하는 메인 메소드를 WebApplicationServer 클래스에 추가함
    - 서버 포트는 8080으로 설정하고, 웹 애플리케이션 디렉토리는 'webapps'로 설정함
    - 서버 시작 후, 서버가 종료될 때까지 대기하도록 구현함

--- mvc-practice/src/main/java/fc/complete/mvc/WebApplicationServer.java
+++ mvc-practice/src/main/java/fc/complete/mvc/WebApplicationServer.java
@@ -1,4 +1,24 @@
 package fc.complete.mvc;
 
+import org.apache.catalina.startup.Tomcat;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+
 public class WebApplicationServer {
+    private static final Logger log = LoggerFactory.getLogger(WebApplicationServer.class);
+    public static void main(String[] args) throws Exception {
+        // /WEB-INF/classes
+        String webappDirLocation = "webapps";
+
+        Tomcat tomcat = new Tomcat();
+        tomcat.setPort(8080);
+
+        tomcat.addWebapp("/", new File(webappDirLocation).getAbsolutePath());
+        log.info("configuring app with basedir: {}", new File("./").getAbsolutePath());
+
+        tomcat.start();
+        tomcat.getServer().await();
+    }
 }
